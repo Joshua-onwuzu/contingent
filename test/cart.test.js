cb0c336e14aa1d45c6db8897d07bf61112a1f2fd
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-10-18 09:13:23 +0100

    add test case for step 2

--- test/cart.test.js
+++ test/cart.test.js
@@ -12,3 +12,15 @@ test('cart should contain 5 Dove Soaps each with a unit price of 39.99 and total
     assert.strictEqual(totalCartPrice, 199.95)
 
 })
+
+
+test('cart should contain 8 Dove Soaps each with a unit price of 39.99 and total price should equal 319.92', () => {
+    const cart = new Cart([{dove: 39.99}])
+    cart.add('dove', 5)
+    cart.add('dove', 3)
+    const cartItems = cart.getItems()
+    const totalCartItems = cartItems.products.length
+    const totalCartPrice = cartItems.totalPrice
+    assert.strictEqual(totalCartItems, 8)
+    assert.strictEqual(totalCartPrice, 319.92)
+})
