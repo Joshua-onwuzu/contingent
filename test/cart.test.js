c845d2c151af71fe729c01e601e3e117fe29c327
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-10-18 09:37:43 +0100

    add test case to calculate  tax rate  multiple items

--- test/cart.test.js
+++ test/cart.test.js
@@ -3,8 +3,8 @@ const assert = require('node:assert');
 const { Cart } = require('../src/cart.js')
 
 test('cart should contain 5 Dove Soaps each with a unit price of 39.99 and total price should equal 199.95', () => {
-    const cart = new Cart([{dove: 39.99}])
-    cart.add('dove', 5)
+    const cart = new Cart([{'Dove Soap': 39.99}])
+    cart.add('Dove Soap', 5)
     const cartItems = cart.getItems()
     const totalCartItems = cartItems.products.length
     const totalCartPrice = cartItems.totalPrice
@@ -15,12 +15,31 @@ test('cart should contain 5 Dove Soaps each with a unit price of 39.99 and total
 
 
 test('cart should contain 8 Dove Soaps each with a unit price of 39.99 and total price should equal 319.92', () => {
-    const cart = new Cart([{dove: 39.99}])
-    cart.add('dove', 5)
-    cart.add('dove', 3)
+    const cart = new Cart([{'Dove Soap': 39.99}])
+    cart.add('Dove Soap', 5)
+    cart.add('Dove Soap', 3)
     const cartItems = cart.getItems()
     const totalCartItems = cartItems.products.length
     const totalCartPrice = cartItems.totalPrice
     assert.strictEqual(totalCartItems, 8)
     assert.strictEqual(totalCartPrice, 319.92)
+})
+
+test('it should calculate the tax rate of the shopping cart with multiple items', () => {
+    const inventory = [{'Dove Soap': 39.99}, {'Axe Deo': 99.99 }]
+    const options = {tax_rate: 12.5}
+    const cart = new Cart(inventory, options)
+    cart.add('Dove Soap', 2)
+    cart.add('Axe Deo', 2)
+    const cartItems = cart.getItems()
+    const products = cartItems.products
+    const has2DoveSoapsEntries = products.filter(product => Object.keys(product)[0] === 'Dove Soap').length === 2
+    const has2DAxeDeosEntries = products.filter(product => Object.keys(product)[0] === 'Axe Deo').length === 2
+    const totalSalesTax = cartItems.totalSalesTax
+    const totalCartPrice = cartItems.totalCartPrice
+    assert.strictEqual(has2DoveSoapsEntries, true)
+    assert.strictEqual(has2DAxeDeosEntries, true)
+    assert.strictEqual(totalCartPrice, 319.92)
+    assert.strictEqual(totalSalesTax, 314.96)
+    
 })
